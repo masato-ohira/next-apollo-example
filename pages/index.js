0a6afd55c61b2423f6ba7e5867a831ac2e9850cf
Masato Ohira <ohira.ekzm@gmail.com>
2022-01-20 09:27:31 +0900

    init

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+
+import queryGraphql from '../shared/query-graphql'
+
+export default function UserListing({ users }) {
+  return (
+    <div>
+      <h1>User Listing</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.username}>
+            <Link href="/[username]" as={`/${user.username}`}>
+              <a>{user.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const { users } = await queryGraphql(`
+    query {
+      users {
+        id
+        name
+        username
+      }
+    }
+  `)
+  return { props: { users } }
+}
